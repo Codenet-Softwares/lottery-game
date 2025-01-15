f76f4ec3f0a79699f6807f5e17d63608bb7f326b
Codenetsoftwares <123172062+Codenetsoftwares@users.noreply.github.com>
2025-01-15 11:46:12 +0530

    Revert "fix"

--- backend/routes/ticket.route.js
+++ backend/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { string } from '../constructor/string.js';
-import { geTicketRange, getIsactiveMarket, saveTicketRange } from '../controllers/ticket.controller.js';
+import { geTicketRange, saveTicketRange } from '../controllers/ticket.controller.js';
 import { authorize } from '../middlewares/auth.js';
 import { validateTicketRange } from '../utils/commonSchema.js';
 import customErrorHandler from '../utils/customErrorHandler.js';
@@ -7,5 +7,4 @@ import customErrorHandler from '../utils/customErrorHandler.js';
 export const ticketRoute = (app) => {
   app.post('/api/generate-ticket', validateTicketRange, customErrorHandler, authorize([string.Admin]), saveTicketRange);
   app.get('/api/get-range', geTicketRange);
-  app.get('/api/get-active-market', getIsactiveMarket);
 };
