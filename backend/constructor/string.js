5d7809fc883c458a93e529e33f2d584a9783ae3a
sumancodenet <sumansarkar.codenet8080@gmail.com>
2025-03-11 15:21:19 +0530

    *

--- backend/constructor/string.js
+++ backend/constructor/string.js
@@ -4,6 +4,8 @@ class stringConst {
   this.User = 'user';
   this.SubAdmin = 'subAdmin';
   this.WinLottery = 'win-Lottery-Result';
+  this.resultView = 'result-View';
+  this.winAnalytics = 'win-Analytics';
   }
 }
 
