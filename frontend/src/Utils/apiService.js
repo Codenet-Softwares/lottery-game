a6d860090b81f1c5cb06e814985ab02274647bf8
tamoghna-10 <137286113+tamoghna-10@users.noreply.github.com>
2025-05-28 16:11:54 +0530

    minor fix again

--- frontend/src/Utils/apiService.js
+++ frontend/src/Utils/apiService.js
@@ -554,7 +554,7 @@ export async function PrizeValidationMarkets(body = {}, isToast = false) {
   try {
     const callParams = await getAuthCallParams(strings.GET, body, isToast);
     const response = await makeCall(
-      ` ${urls.compareValidationMarkets}?search=${body.search}`,
+      ` ${urls.compareValidationMarkets}?page=${body.page}&limit=${body.limit}&search=${body.search}`,
       callParams,
       isToast
     );
