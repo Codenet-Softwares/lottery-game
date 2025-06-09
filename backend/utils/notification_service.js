9c1c4f79ddb728287e827d212b89085d71f7239f
himanshu10-kashyap <himanshudbg10@gmail.com>
2025-06-09 20:40:25 +0530

    Update Firebase service account import path in notification service

--- backend/utils/notification_service.js
+++ backend/utils/notification_service.js
@@ -1,7 +1,7 @@
 import { initializeApp, getApps } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import userSchema from "../models/user.model.js";
-import serviceAccount from "../cg-firebase.json" assert { type: "json" };
+import serviceAccount from "../lottery-firebase.json" assert { type: "json" };
 
 export default class NotificationService {
   static FIREBASE_APP = null;
